028b4c626021148f4831dde1706df5d630ac1dcd
Alex Gázquez <ercic@Zero-Two>
2018-11-28 12:23:17 +0100

    Clase Contacto con sus atributos

---
+++ src/test/java/org/iesalandalus/programacion/agenda/modelo/Contacto.java
@@ -0,0 +1,11 @@
+package org.iesalandalus.programacion.agenda.modelo;
+
+public class Contacto {
+
+	private static final String ER_TELEFONO;
+	private static final String ER_CORREO;
+	private String nombre;
+	private String telefono;
+	private String correo;
+	
+}
