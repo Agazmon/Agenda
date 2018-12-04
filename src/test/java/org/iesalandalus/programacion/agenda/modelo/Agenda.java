4f796e05de7ed98f081fa8617188d1f07239c94b
Alex Gázquez <ercic@Zero-Two>
2018-12-04 18:28:56 +0100

    Creado el constructor y los metodos get

--- src/test/java/org/iesalandalus/programacion/agenda/modelo/Agenda.java
+++ src/test/java/org/iesalandalus/programacion/agenda/modelo/Agenda.java
@@ -1,6 +1,25 @@
 package org.iesalandalus.programacion.agenda.modelo;
 
 public class Agenda {
-private static final int MAX_CONTACTOS;
-private int numContactos;
+	private static final int MAX_CONTACTOS; /*Max contactos tampoco se a que se refiere por ahora*/
+	private int numContactos;
+	Contacto[] contactosAgenda;
+
+	public Agenda() {
+
+		/* todavía no tengo clara la funcion del constructor Agenda */
+	}
+
+	public Contacto[] getContactos() {
+		Contacto[] contactosCopia;
+		contactosCopia = new Contacto[numContactos];
+		for (int p = 0; p < numContactos; p++) {
+			contactosCopia[p] = contactosAgenda[p];
+		}
+		return contactosCopia;
+	}
+
+	public int getNumContactos() {
+		return numContactos;
+	}
 }
